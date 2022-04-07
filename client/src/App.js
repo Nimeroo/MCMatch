4b4c31945f00e5530d7c1ab5fd38fe955c1190dd
Nimeroo <girou.joe@gmail.com>
2022-04-07 03:20:44 -0400

    Dom elements for pre game screen added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
+import { useState } from "react";
+import { GameTitle } from "./Components/GameTitle/GameTitle";
+import { DifficultySelector } from "./Components/DifficultySelector/DifficultySelector";
 
 function App() {
-  return <div className="App"></div>;
+  const [difficulty, setDifficulty] = useState("");
+
+  const preGame = (
+    <div>
+      <DifficultySelector setDifficulty={setDifficulty} />
+      <div>
+        <button>Start Game</button>
+        <button>Records</button>
+      </div>
+    </div>
+  );
+
+  return (
+    <div className="App">
+      <GameTitle />
+      <div></div>
+    </div>
+  );
 }
 
 export default App;
