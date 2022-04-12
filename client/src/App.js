7a3687efb10a178b065166a2f3380b4d1c1acd37
Nimeroo <girou.joe@gmail.com>
2022-04-12 06:10:02 -0400

    in game dom elements rendered and state passed to components

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,62 @@
 import "./App.css";
 import { useState } from "react";
+import { gameItems } from "./Util/gameItems";
+import { gameConfig } from "./Util/gamegenerator";
 import { GameTitle } from "./Components/GameTitle/GameTitle";
+import { GameGrid } from "./Components/GameGrid/GameGrid";
 import { DifficultySelector } from "./Components/DifficultySelector/DifficultySelector";
 
 function App() {
-  const [difficulty, setDifficulty] = useState("");
+  const [difficulty, setDifficulty] = useState("easy");
+  const [gameState, setGameState] = useState("off");
+  const [itemList, setItemList] = useState([]);
 
+  const fetchItems = async () => {
+    const items = await gameConfig(gameItems, difficulty);
+    setItemList(items);
+    console.log(itemList);
+  };
+
+  // Pre-game screen //
   const preGame = (
     <div>
-      <DifficultySelector setDifficulty={setDifficulty} />
+      <DifficultySelector setDifficulty={setDifficulty} fetchItems={fetchItems} />
       <div>
-        <button>Start Game</button>
+        <button
+          onClick={() => {
+            setGameState("running");
+            fetchItems(); 
+          }}
+        >
+          Start Game
+        </button>
         <button>Records</button>
       </div>
     </div>
   );
 
+  // In-game screen //
+  const inGame = (
+    <div>
+      <div>
+        <GameGrid difficulty={difficulty} itemList={itemList} />
+      </div>
+      <button onClick={() => setGameState("off")}>Exit Game</button>
+    </div>
+  );
+
+  const gameStatus = () => {
+    if (gameState === "off") {
+      return preGame;
+    } else if (gameState === "running") {
+      return inGame;
+    }
+  };
+
   return (
     <div className="App">
       <GameTitle />
-      <div></div>
+      <div>{gameStatus()}</div>
     </div>
   );
 }
