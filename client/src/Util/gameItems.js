425f80a5310df0cebd73feef9131dfb1de2db35f
Nimeroo <girou.joe@gmail.com>
2022-04-12 06:11:10 -0400

    array formatted

--- client/src/Util/gameItems.js
+++ client/src/Util/gameItems.js
@@ -1,36 +1,193 @@
-
-
 export const gameItems = [
-  { name: "stone", isMatched: false, image: "https://static.wikia.nocookie.net/minecraft/images/d/d4/Stone.png" },
-  { name: "dirt", isMatched: false, image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2f/Dirt.png" },
-  { name: "golden_apple", isMatched: false, image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/5/54/Golden_Apple_JE2_BE2.png" },
-  { name: "diamond_sword", isMatched: false, image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/4/44/Diamond_Sword_JE3_BE3.png" },
-  { name: "diamond_pickaxe", isMatched: false, image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/e/e7/Diamond_Pickaxe_JE3_BE3.png" },
-  { name: "diamond_axe", isMatched: false, image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/4/40/Diamond_Axe_JE3_BE3.png" },
-  { name: "diamond_hoe", isMatched: false, image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/0/04/Diamond_Hoe_JE3_BE3.png" },
-  { name: "diamond_shovel", isMatched: false, image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/e/ec/Diamond_Shovel_JE3_BE3.png" },
-  { name: "diamond", isMatched: false, image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/a/ab/Diamond_JE3_BE3.png" },
-  { name: "gold", isMatched: false, image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/8/8a/Gold_Ingot_JE4_BE2.png" },
-  { name: "obsidian", isMatched: false, image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/9/99/Obsidian_JE3_BE2.png" },
-  { name: "ender_pearl", isMatched: false, image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/f/f6/Ender_Pearl_JE3_BE2.png" },
-  { name: "leather", isMatched: false, image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/6/6d/Leather_JE2_BE2.png" },
-  { name: "carrot", isMatched: false, image: "https://static.wikia.nocookie.net/minecraft/images/6/63/Carrot_Updated.png" },
-  { name: "apple", isMatched: false, image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/a/af/Apple_JE3_BE3.png" },
-  { name: "bone", isMatched: false, image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/3/3a/Bone_JE3_BE2.png" },
-  { name: "rotten_flesh", isMatched: false, image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/a/ac/Rotten_Flesh_JE3_BE2.png" },
-  { name: "gun_powder", isMatched: false, image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/4/40/Gunpowder_JE2_BE2.png" },
-  { name: "bow", isMatched: false, image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/9/99/Bow_JE2_BE1.png" },
-  { name: "bucket", isMatched: false, image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/f/fc/Bucket_JE2_BE2.png" },
-  { name: "egg", isMatched: false, image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/9/96/Egg_JE2_BE2.png" },
-  { name: "saddle", isMatched: false, image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/b/ba/Saddle_JE2_BE2.png" },
-  { name: "emerald", isMatched: false, image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/26/Emerald_JE3_BE3.png" },
-  { name: "redstone", isMatched: false, image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/d/d5/Redstone_Dust_JE1_BE1.png" },
-  { name: "lapis", isMatched: false, image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/4/43/Lapis_Lazuli_JE2_BE2.png" },
-  { name: "piston", isMatched: false, image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2d/Piston_%28U%29_JE1.png" },
-  { name: "iron", isMatched: false, image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/f/fc/Iron_Ingot_JE3_BE2.png" },
-  { name: "diamond_helmet", isMatched: false, image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/7/73/Diamond_Helmet_%28item%29_JE1_BE1.png" },
-  { name: "diamond_chestplate", isMatched: false, image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/e/e3/Diamond_Chestplate_%28item%29_JE1_BE1.png" },
-  { name: "diamond_leggings", isMatched: false, image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/8/8e/Diamond_Leggings_%28item%29_JE1_BE1.png" },
-  { name: "diamond_boots", isMatched: false, image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/f/f0/Diamond_Boots_%28item%29_JE1_BE1.png" },
-  { name: "tnt", isMatched: false, image: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/a/a2/TNT_JE3_BE2.png" },
+  {
+    name: "stone",
+    isMatched: false,
+    image: "https://static.wikia.nocookie.net/minecraft/images/d/d4/Stone.png",
+  },
+  {
+    name: "dirt",
+    isMatched: false,
+    image:
+      "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2f/Dirt.png",
+  },
+  {
+    name: "golden_apple",
+    isMatched: false,
+    image:
+      "https://static.wikia.nocookie.net/minecraft_gamepedia/images/5/54/Golden_Apple_JE2_BE2.png",
+  },
+  {
+    name: "diamond_sword",
+    isMatched: false,
+    image:
+      "https://static.wikia.nocookie.net/minecraft_gamepedia/images/4/44/Diamond_Sword_JE3_BE3.png",
+  },
+  {
+    name: "diamond_pickaxe",
+    isMatched: false,
+    image:
+      "https://static.wikia.nocookie.net/minecraft_gamepedia/images/e/e7/Diamond_Pickaxe_JE3_BE3.png",
+  },
+  {
+    name: "diamond_axe",
+    isMatched: false,
+    image:
+      "https://static.wikia.nocookie.net/minecraft_gamepedia/images/4/40/Diamond_Axe_JE3_BE3.png",
+  },
+  {
+    name: "diamond_hoe",
+    isMatched: false,
+    image:
+      "https://static.wikia.nocookie.net/minecraft_gamepedia/images/0/04/Diamond_Hoe_JE3_BE3.png",
+  },
+  {
+    name: "diamond_shovel",
+    isMatched: false,
+    image:
+      "https://static.wikia.nocookie.net/minecraft_gamepedia/images/e/ec/Diamond_Shovel_JE3_BE3.png",
+  },
+  {
+    name: "diamond",
+    isMatched: false,
+    image:
+      "https://static.wikia.nocookie.net/minecraft_gamepedia/images/a/ab/Diamond_JE3_BE3.png",
+  },
+  {
+    name: "gold",
+    isMatched: false,
+    image:
+      "https://static.wikia.nocookie.net/minecraft_gamepedia/images/8/8a/Gold_Ingot_JE4_BE2.png",
+  },
+  {
+    name: "obsidian",
+    isMatched: false,
+    image:
+      "https://static.wikia.nocookie.net/minecraft_gamepedia/images/9/99/Obsidian_JE3_BE2.png",
+  },
+  {
+    name: "ender_pearl",
+    isMatched: false,
+    image:
+      "https://static.wikia.nocookie.net/minecraft_gamepedia/images/f/f6/Ender_Pearl_JE3_BE2.png",
+  },
+  {
+    name: "leather",
+    isMatched: false,
+    image:
+      "https://static.wikia.nocookie.net/minecraft_gamepedia/images/6/6d/Leather_JE2_BE2.png",
+  },
+  {
+    name: "carrot",
+    isMatched: false,
+    image:
+      "https://static.wikia.nocookie.net/minecraft/images/6/63/Carrot_Updated.png",
+  },
+  {
+    name: "apple",
+    isMatched: false,
+    image:
+      "https://static.wikia.nocookie.net/minecraft_gamepedia/images/a/af/Apple_JE3_BE3.png",
+  },
+  {
+    name: "bone",
+    isMatched: false,
+    image:
+      "https://static.wikia.nocookie.net/minecraft_gamepedia/images/3/3a/Bone_JE3_BE2.png",
+  },
+  {
+    name: "rotten_flesh",
+    isMatched: false,
+    image:
+      "https://static.wikia.nocookie.net/minecraft_gamepedia/images/a/ac/Rotten_Flesh_JE3_BE2.png",
+  },
+  {
+    name: "gun_powder",
+    isMatched: false,
+    image:
+      "https://static.wikia.nocookie.net/minecraft_gamepedia/images/4/40/Gunpowder_JE2_BE2.png",
+  },
+  {
+    name: "bow",
+    isMatched: false,
+    image:
+      "https://static.wikia.nocookie.net/minecraft_gamepedia/images/9/99/Bow_JE2_BE1.png",
+  },
+  {
+    name: "bucket",
+    isMatched: false,
+    image:
+      "https://static.wikia.nocookie.net/minecraft_gamepedia/images/f/fc/Bucket_JE2_BE2.png",
+  },
+  {
+    name: "egg",
+    isMatched: false,
+    image:
+      "https://static.wikia.nocookie.net/minecraft_gamepedia/images/9/96/Egg_JE2_BE2.png",
+  },
+  {
+    name: "saddle",
+    isMatched: false,
+    image:
+      "https://static.wikia.nocookie.net/minecraft_gamepedia/images/b/ba/Saddle_JE2_BE2.png",
+  },
+  {
+    name: "emerald",
+    isMatched: false,
+    image:
+      "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/26/Emerald_JE3_BE3.png",
+  },
+  {
+    name: "redstone",
+    isMatched: false,
+    image:
+      "https://static.wikia.nocookie.net/minecraft_gamepedia/images/d/d5/Redstone_Dust_JE1_BE1.png",
+  },
+  {
+    name: "lapis",
+    isMatched: false,
+    image:
+      "https://static.wikia.nocookie.net/minecraft_gamepedia/images/4/43/Lapis_Lazuli_JE2_BE2.png",
+  },
+  {
+    name: "piston",
+    isMatched: false,
+    image:
+      "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2d/Piston_%28U%29_JE1.png",
+  },
+  {
+    name: "iron",
+    isMatched: false,
+    image:
+      "https://static.wikia.nocookie.net/minecraft_gamepedia/images/f/fc/Iron_Ingot_JE3_BE2.png",
+  },
+  {
+    name: "diamond_helmet",
+    isMatched: false,
+    image:
+      "https://static.wikia.nocookie.net/minecraft_gamepedia/images/7/73/Diamond_Helmet_%28item%29_JE1_BE1.png",
+  },
+  {
+    name: "diamond_chestplate",
+    isMatched: false,
+    image:
+      "https://static.wikia.nocookie.net/minecraft_gamepedia/images/e/e3/Diamond_Chestplate_%28item%29_JE1_BE1.png",
+  },
+  {
+    name: "diamond_leggings",
+    isMatched: false,
+    image:
+      "https://static.wikia.nocookie.net/minecraft_gamepedia/images/8/8e/Diamond_Leggings_%28item%29_JE1_BE1.png",
+  },
+  {
+    name: "diamond_boots",
+    isMatched: false,
+    image:
+      "https://static.wikia.nocookie.net/minecraft_gamepedia/images/f/f0/Diamond_Boots_%28item%29_JE1_BE1.png",
+  },
+  {
+    name: "tnt",
+    isMatched: false,
+    image:
+      "https://static.wikia.nocookie.net/minecraft_gamepedia/images/a/a2/TNT_JE3_BE2.png",
+  },
 ];
