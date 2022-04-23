ab5be134de6fa3e5fe427a06a227c412b4deffe0
Nimeroo <girou.joe@gmail.com>
2022-04-23 03:00:21 -0400

    Difficulty selector CSS completed

--- client/src/Components/DifficultySelector/DifficultySelector.jsx
+++ client/src/Components/DifficultySelector/DifficultySelector.jsx
@@ -1,19 +1,55 @@
 import React from "react";
+import "./DifficultySelector.css";
 
-export const DifficultySelector = ({ setDifficulty }) => {
+export const DifficultySelector = ({ setDifficulty, difficulty }) => {
+
+  // Changes class name for selected difficulty // 
+  const selectedCardStyle = (className) => {
+    if (difficulty === "Easy") {
+      if (className[className.length - 1] === "y") {
+        return className + "--selected";
+      } else return className;
+    }
+    if (difficulty === "Intermediate") {
+      if (className[className.length - 1] === "e") {
+        return className + "--selected";
+      } else return className;
+    }
+    if (difficulty === "Hard") {
+      if (className[className.length - 1] === "d") {
+        return className + "--selected";
+      } else return className;
+    }
+  };
+
   return (
-    <div>
-      <div onClick={() => setDifficulty("Easy")}>
-        <h3>Easy</h3>
-        <h5>(8 Pairs)</h5>
+    <div className="difficulties">
+      <div
+        className={selectedCardStyle("difficulties__card-easy")}
+        onClick={() => setDifficulty("Easy")}
+      >
+        <div className="difficulties__card__container">
+          <h3 className="difficulties__card__title">Easy</h3>
+          <h5 className="difficulties__card__note">(8 Pairs)</h5>
+        </div>
       </div>
-      <div onClick={() => setDifficulty("Intermediate")}>
-        <h3>Intermediate</h3>
-        <h5>(16 Pairs)</h5>
+      <div
+        className={selectedCardStyle("difficulties__card-intermediate")}
+        onClick={() => setDifficulty("Intermediate")}
+      >
+        <div className="difficulties__card__container">
+          <h3 className="difficulties__card__title">Intermediate</h3>
+          <h5 className="difficulties__card__note">(16 Pairs)</h5>
+        </div>
       </div>
-      <div onClick={() => setDifficulty("Hard")}>
-        <h3>Hard</h3>
-        <h5>(32 Pairs)</h5>
+      <div
+        className={selectedCardStyle("difficulties__card-hard")}
+        onClick={() => setDifficulty("Hard")}
+      >
+        <div className="difficulties__card__container">
+          <h3 className="difficulties__card__title">Hard</h3>
+          <h5 className="difficulties__card__note">(32 Pairs)</h5>
+        </div>
       </div>
     </div>
   );
